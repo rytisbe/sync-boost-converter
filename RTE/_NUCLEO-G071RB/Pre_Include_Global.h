476f76af153ceda790f1e1adbd5904e54f74e5c5
Rytis Beinarys <30464429+rytisbe@users.noreply.github.com>
2022-08-01 15:31:40 +0100

    first commit

---
+++ RTE/_NUCLEO-G071RB/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'sync_boost_converter' 
+ * Target:  'NUCLEO-G071RB' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil.Standalone::Device:Startup:1.0.0 */
+#define USE_HAL_DRIVER
+          #define USE_FULL_LL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
---
+++ main.c
@@ -0,0 +1,24 @@
+#include "stm32g0xx.h"
+
+void SystemClockInit(void);
+
+int main(){
+	SystemClockInit();
+	// do nothing
+}
+
+void SystemClockInit(void){
+	/*
+	- upon system reset, the HSISYS clock derived from HSI16 oscillator is selected as system clock
+	
+	*/
+	
+	RCC->CR &= (uint32_t)(~RCC_CR_PLLON);								// disable PLL
+	while((RCC->CR & RCC_CR_PLLRDY) != 0){}							// wait till PLLRDY is cleared				
+	RCC->CFGR |= (uint32_t)(RCC_PLLCFGR_PLLM_0);				// PLL division factor M = 2
+	RCC->CFGR |= (uint32_t)(RCC_PLLCFGR_PLLN_0);				// PLL multiplication factor N = 8	
+	RCC->CR 	|= (uint32_t)(RCC_CR_PLLON);							// enable the PLL 
+	RCC->PLLCFGR |= (uint32_t)(RCC_PLLCFGR_PLLREN);			// enable PLLRCLK
+	RCC->CFGR	|= (uint32_t)(RCC_CFGR_SW_1);							// use PLLRCLK as system clock
+		
+}
