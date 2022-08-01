f238c567b53b1f73bbed86a85dd97f1b04a1ba7f
Rytis Beinarys <30464429+rytisbe@users.noreply.github.com>
2022-08-01 20:07:45 +0100

    add clock init function for PLL at 64 MHz

--- main.c
+++ main.c
@@ -2,24 +2,30 @@
 
 void SystemClockInit(void);
 
-int main(){
-	SystemClockInit();
-	// do nothing
+int main()
+{
+	while (1)
+	{
+		SystemClockInit();
+		// do nothing
+	}
+
 }
 
-void SystemClockInit(void){
-	/*
-	- upon system reset, the HSISYS clock derived from HSI16 oscillator is selected as system clock
+void SystemClockInit(void)
+{
+	// upon system reset, the HSISYS clock derived from HSI16 oscillator is selected as system clock
+
+	if ((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_1)	
+	{
+		return;																								// PLL is already used as system clock
+	}	
 	
-	*/
-	
-	RCC->CR &= (uint32_t)(~RCC_CR_PLLON);								// disable PLL
-	while((RCC->CR & RCC_CR_PLLRDY) != 0){}							// wait till PLLRDY is cleared				
-	RCC->CFGR |= (uint32_t)(RCC_PLLCFGR_PLLM_0);				// PLL division factor M = 2
-	RCC->CFGR |= (uint32_t)(RCC_PLLCFGR_PLLN_0);				// PLL multiplication factor N = 8	
-	RCC->CR 	|= (uint32_t)(RCC_CR_PLLON);							// enable the PLL 
-	RCC->PLLCFGR |= (uint32_t)(RCC_PLLCFGR_PLLREN);			// enable PLLRCLK
-	RCC->CFGR	|= (uint32_t)(RCC_CFGR_SW_1);							// use PLLRCLK as system clock
-		
+	RCC->CR &= (uint32_t)(~RCC_CR_PLLON);										// disable PLL
+	while((RCC->CR & RCC_CR_PLLRDY) != 0){}									// wait till PLLRDY is cleared				
+	RCC->PLLCFGR 	 = (uint32_t)(0x00000112);								// PLL input clock source = HSI16 | M = *2 |  N = /8 --> 16*2/8 = 64 MHz
+	RCC->CR 			|= (uint32_t)(RCC_CR_PLLON);							// enable the PLL 
+	RCC->PLLCFGR 	|= (uint32_t)(0x11010000);								// enable PLLRCLK  | PLLQCLK  | PLLPCLK clock output
+	RCC->CFGR			|= (uint32_t)(RCC_CFGR_SW_1);							// use PLLRCLK as system clock
 }
 
